86d0bd8fb74d807c180b783fe95a907fbe6bcf0d
Evgeny Shmakov <sjs-master@yandex.ru>
2018-02-02 02:30:58 +0700

    OMA-7 add geolocation watch/position;

--- src/containers/AppTest.js
+++ src/containers/AppTest.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Dimensions, StyleSheet, View } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 const styles = StyleSheet.create({
@@ -12,12 +12,66 @@ const styles = StyleSheet.create({
 	},
 	map: {
 		flex: 1,
-		...StyleSheet.absoluteFillObject
+        ...StyleSheet.absoluteFillObject
 	}
 });
+const {width, height} = Dimensions.get('window');
 
+const ASPECT_RATIO = width / height;
+const LATTITIDE_DELTA = 0.0922;
+const LONGTITUDE_DELTA = LATTITIDE_DELTA * ASPECT_RATIO;
 export default class App extends Component {
-	componentWillUnmount() {}
+	state = {
+		initialPosition: {
+			latitude: 0,
+			longitude: 0,
+            latitudeDelta: 0,
+            longitudeDelta: 0
+		},
+		markerPosition: {
+            latitude: 0,
+            longitude: 0
+		}
+	};
+	watchID = null;
+
+	componentDidMount() {
+		navigator.geolocation.getCurrentPosition(position => {
+			const lat = parseFloat(position.coords.latitude);
+			const long = parseFloat(position.coords.longitude);
+
+			const initialRegion = {
+                latitude: lat,
+                longitude: long,
+                latitudeDelta: LATTITIDE_DELTA,
+                longitudeDelta: LONGTITUDE_DELTA
+            };
+			console.log('initialRegion:', initialRegion);
+			this.setState({initialPosition: initialRegion});
+            this.setState({markerPosition: initialRegion});
+		},
+		e => { console.log('', e); },
+		{enableHighAccuracy: true, timeout: 10000, maximumAge: 1000});
+		this.watchID = navigator.geolocation.watchPosition(position => {
+                const lat = parseFloat(position.coords.latitude);
+                const long = parseFloat(position.coords.longitude);
+
+                const lastRegion = {
+                    latitude: lat,
+                    longitude: long,
+                    latitudeDelta: LATTITIDE_DELTA,
+                    longitudeDelta: LONGTITUDE_DELTA
+                };
+                console.log('lastRegion:', lastRegion);
+                this.setState({initialPosition: lastRegion});
+                this.setState({markerPosition: lastRegion});
+            },
+            e => { console.log('error::', e); },
+            {enableHighAccuracy: true, timeout: 10000, maximumAge: 1000});
+	}
+    componentWillUnmount() {
+        navigator.geolocation.clearWatch(this.watchID);
+	}
 	render() {
 		return (
 			<View style={styles.container}>
@@ -25,16 +79,11 @@ export default class App extends Component {
 					style={styles.map}
 					provider={PROVIDER_GOOGLE}
 					minZoomLevel={0}
-					maxZoomLevel={4}
+					// maxZoomLevel={4}
 					showsUserLocation
 					showsMyLocationButton
 					zoomEnabled
-					region={{
-						latitude: 37.78825,
-						longitude: -122.4324,
-						latitudeDelta: 0.015,
-						longitudeDelta: 0.0121
-					}}
+					region={this.state.initialPosition}
 				/>
 			</View>
 		);
