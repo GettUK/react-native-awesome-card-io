c162865b53c9b9d66dceabba97959e85ffbe6e23
Evgeny Shmakov <sjs-master@yandex.ru>
2018-02-06 01:37:11 +0700

    add link react-native-vector-icons

--- android/app/src/main/java/com/oteapp/MainApplication.java
+++ android/app/src/main/java/com/oteapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.oteapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.horcrux.svg.SvgPackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.airbnb.android.react.maps.MapsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new SvgPackage(),
             new RNI18nPackage(),
             new MapsPackage()
