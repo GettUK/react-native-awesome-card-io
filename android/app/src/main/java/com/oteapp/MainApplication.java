78149f3c833cb3875d4c33371c179903a122dab1
Evgeny Shmakov <sjs-master@yandex.ru>
2018-02-06 01:44:32 +0700

    add link linear-gradient

--- android/app/src/main/java/com/oteapp/MainApplication.java
+++ android/app/src/main/java/com/oteapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.oteapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.horcrux.svg.SvgPackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LinearGradientPackage(),
             new VectorIconsPackage(),
             new SvgPackage(),
             new RNI18nPackage(),
