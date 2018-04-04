8480480ba217177f7de73e34aa44f18df0983c6a
Evgeny Shmakov <sjs-master@yandex.ru>
2018-04-04 22:50:14 +0700

    OMA-116 Order Creation: OT black taxi icon is missing
    Description:
    
     - Add OT black taxi icon
     - removed unused icons
    
    References:
    
     - https://gett-uk.atlassian.net/browse/OMA-116

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,15 +1,13 @@
 /* eslint-disable global-require */
 const imagesApp = {
   carTypes: {
+    OTBlackTaxi: require('./png/application/OTBlackTaxi.png'),
     BlackTaxi: require('./png/application/BlackTaxi.png'),
     Standard: require('./png/application/Standard.png'),
     BlackTaxiXL: require('./png/application/BlackTaxiXL.png'),
     Exec: require('./png/application/Exec.png'),
     MPV: require('./png/application/MPV.png')
   },
-  aupairLarge: require('./png/application/AupairLarge.png'),
-  eyeIcon: require('./png/application/EyeIconSmall.png'),
-  eyeIconActive: require('./png/application/EyeIconSmallActive.png'),
   clear: require('./png/application/clear-icon.png'),
   loginBg: require('./png/application/login-bg.jpg'),
   orderMap: require('./png/application/orderTempMap.jpg'),
