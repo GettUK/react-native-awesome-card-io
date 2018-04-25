1ebde637249b3ff3181df27f89147300c7ad154a
Evgeny Shmakov <sjs-master@yandex.ru>
2018-04-25 16:49:58 +0700

    OMA-202 add checkboxes to login page
    Description:
    
     - add on login ui checboxes
     - action/reduser
     - SwitchItem component
    
    References:
    
     - https://gett-uk.atlassian.net/browse/OMA-202

---
+++ src/components/SwitchItem/style.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginVertical: 10
+  },
+  label: {
+    color: '#fff',
+    fontSize: 17,
+    lineHeight: 22
+  }
+});
+
+export default styles;
