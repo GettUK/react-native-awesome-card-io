42d9f23d0d31d86f48bc19fcef65fc9ebec8e6e9
AleksandrZhukov <aleksandr.a.zhukov@gmail.com>
2018-05-06 00:03:40 +0300

    OMA-238 New zoom value for current location on location button press
    Description:
    - set new default value
    
    References:
    - https://gett-uk.atlassian.net/browse/OMA-238

--- src/utils/map.js
+++ src/utils/map.js
@@ -3,5 +3,5 @@ import { Dimensions } from 'react-native';
 const { width, height } = Dimensions.get('window');
 
 const ASPECT_RATIO = width / height;
-export const LATTITIDE_DELTA = 0.0125;
+export const LATTITIDE_DELTA = 0.004;
 export const LONGTITUDE_DELTA = LATTITIDE_DELTA * ASPECT_RATIO;
