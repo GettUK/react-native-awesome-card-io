b9c6a682bab6bb37f0480b2588556668577ddfac
AleksandrZhukov <aleksandr.a.zhukov@gmail.com>
2018-07-23 19:33:56 +0300

    OMA-608 Android: pointer is invisible on some fields
    Description:
    - set default selectionColor
    
    References:
    https://gett-uk.atlassian.net/browse/OMA-608

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import { createStore } from 'store';
 import { withNetworkConnectivity } from 'react-native-offline';
-import 'utils/fixAndroidText';
+import 'utils/fixAndroidElements';
 import App from 'containers/App';
 import axios from 'utils/axios';
 
