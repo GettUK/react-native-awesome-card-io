a6c1d1e3708c263b7eb66df0f59caf3566f7de7c
Evgeny Shmakov <sjs-master@yandex.ru>
2018-08-10 19:17:15 +0700

    OMA-697 Can save secondary phone number shorter than 10 symbols
    Description:
    
    - add new rule for empty fild with minLenght param
    
    References:
    https://gett-uk.atlassian.net/browse/OMA-697

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,9 +1,16 @@
 import validate from 'validate.js';
 import moment from 'moment';
+import { strings } from 'locales';
 
 validate.validators.expired = ({ year, month } = {}) => {
   const creditCardDate = moment(`${year}${month}`, 'YYYYMM');
   return creditCardDate.isValid() && (moment() > creditCardDate.add(1, 'months')) ? 'is expired' : null;
+};
+
+validate.validators.minLengthWithAllowEmpty = (value, minlength) => {
+  if (!value) return null;
+  if (value.length < minlength) return strings('fieldValidation.phone.length');
+  return null;
 };
 
 export default validate;
